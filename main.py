e68f5239ff3e1d588356cef81d741e845d7e8341
yen-yang <79854932+yen-yang@users.noreply.github.com>
2021-03-02 21:22:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+''' main '''
+# 使用自建的套件 myMath 來計算使用者輸入的五個數的平均值
+from cbMath import myArithmetic,myCalcArea,myStatistics
+a = 0
+x = 0
+sum = 0
+
+while x < 5:
+
+    x += 1
+
+    b = float(input("輸入一個數:"))
+
+    sum = myArithmetic.myAdd(a,b)
+
+    a = sum
+
+print(myArithmetic. myDiv(a,x)) 
+
+    
+
+    
+
+
+    
+
+
